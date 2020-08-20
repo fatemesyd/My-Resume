dc2345ba799abd05de13f6c6d148c822195b8a2a
fatemesyd <ftmews@gmail.com>
2020-08-20 17:12:07 +0430

    Updates

---
+++ precache-manifest.6ff0141cdb52861a01284e9a033bfe2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834ae86d46c6a1bb8d2b70f75cc1656c",
+    "url": "/My-Resume/index.html"
+  },
+  {
+    "revision": "17d900d646d57783b481",
+    "url": "/My-Resume/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "03ba02b360d82dd98077",
+    "url": "/My-Resume/static/css/main.0a098918.chunk.css"
+  },
+  {
+    "revision": "17d900d646d57783b481",
+    "url": "/My-Resume/static/js/2.648048e8.chunk.js"
+  },
+  {
+    "revision": "d97888cb28706be86d280413baaeacae",
+    "url": "/My-Resume/static/js/2.648048e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ba02b360d82dd98077",
+    "url": "/My-Resume/static/js/main.c7fff3fe.chunk.js"
+  },
+  {
+    "revision": "f115f185c2fe553c63b7",
+    "url": "/My-Resume/static/js/runtime-main.585f0982.js"
+  }
+]);
